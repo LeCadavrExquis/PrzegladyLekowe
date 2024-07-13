b150642f2d0e172ad6deecdf45c36e91eb9aa515
DupaPsia1 <f.p.dudek@gmail.com>
2024-07-14 01:19:09 +0200

    Dashboard update

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,6 +1,44 @@
-const Dashboard = () => {
-    // TODO
-    return <h1>Not implemented</h1>
-}
+import React, { useState } from 'react';
 
-export {Dashboard}
+import { Doctor } from './Doctor';
+import { User } from './User';
+import { RoleChoice } from './RoleChoice';
+import { UserForm } from './UserForm';
+
+
+const Dashboard = () => {
+
+  const [currentComponent, setCurrentComponent] = useState('roleChoice');
+
+  const switchComponent = (component) => {
+    setCurrentComponent(component);
+  };
+
+  const renderComponent = () => {
+    switch (currentComponent) {
+
+        case 'roleChoice':
+            return <RoleChoice switchComponent={switchComponent} />;
+
+        case 'doctor':
+            return <Doctor switchComponent={switchComponent} />;
+
+        case 'user':
+            return <User switchComponent={switchComponent} />;
+
+        case 'userForm':
+            return <UserForm switchComponent={switchComponent} />;
+        
+        default:
+            return null;
+    }
+  }
+
+  return (
+    <div>
+       {renderComponent()}
+    </div>
+  );
+};
+
+export {Dashboard};
